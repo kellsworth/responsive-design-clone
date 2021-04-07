3093125bdb365739b3888fa433fa4328bd2d4bcf
Karen - iMac <kellsworth628@gmail.com>
2021-04-07 15:54:28 -0600

    completed

--- responsive-design-clone/src/App.js
+++ responsive-design-clone/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Start Bootstrap</h2>
+        <nav className="header-nav">
+          <h3>About</h3>
+          <h3>Projects</h3>
+          <h3>Contact</h3>
+        </nav>
       </header>
+      <main>
+        <h1>GRAYSCALE</h1>
+        <p>A free, responsive, one page Bootstrap theme <br />created by Start Bootstrap.</p>
+        <button>GET STARTED</button>
+      </main>
     </div>
   );
 }
